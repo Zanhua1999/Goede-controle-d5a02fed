0228a3902cebdbc92095e319353b35a707012997
Zanhua1999 <skyair886@gmail.com>
2020-02-25 11:38:59 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,26 @@
+var points = 0;
+
+function my_func() {
+var answer = ["Parijs", "8", "Ijsselmeer"];
+var arr = [];
+var ans1 = document.getElementById("fr").value;
+var ans2 = document.getElementById("sp").value;
+var ans3 = document.getElementById("nl").value;
+arr.push(ans1, ans2, ans3);
+for (let i= 0; i < arr.length; i++) {
+    if (answer[i] == arr[i]) {
+        points++;
+    }else{
+        
+    }
+}
+if (points == 3) {
+    console.log('Je hebt alles goed');
+}else {
+    console.log('Je hebt een fout');
+}points = 0;
+}
+
+
+
+
